bd7177e84ceec4e361728e6579bfd21860d2f6d4
Felipe Andrade <feandrad@gmail.com>
2017-05-30 14:45:46 -0400

    express config

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-
 var express = require('express');
 var app = express();
 var router = express.Router();
+
 var path = __dirname + '/views/';
 
-router.use(function (req,res,next) {
-    console.log("/" + req.method);
-    next();
+app.use('/', router);
+
+router.get('/', function (req, res) {
+    res.sendFile(path + 'index.html');
 });
 
-router.get("/",function(req,res){
-    res.sendFile(path + "index.html");
-});
-
-app.use("*",function(req,res){
-    res.sendFile(path + "404.html");
+app.use('*', function (req, res) {
+    res.send('Error 404: Not Found!');
 });
 
 app.listen(3000, function () {
-    console.log("Live at Port 3000");
+    console.log("Webservice running at Port 3000");
 });
